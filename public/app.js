01a6d6c41d81237ff89d272a7655766bee0a6778
Crhistian <crhistiandeoliveira@gmail.com>
2021-12-15 05:24:16 -0300

    fixed leaderboard

--- public/app.js
+++ public/app.js
@@ -98,12 +98,16 @@ function backToStart() {
     document.getElementById("settings").style.display = "none";
     document.getElementById("buttons").style.display = "flex";
     document.getElementById("title").style.display = "none";
+    document.getElementById("counterText").style.display = "none";
+    document.getElementById("counterText").innerHTML = `<span id="counter" class="h4 ms-2"data-count="0"></span>`;
+    document.getElementById("timerText").style.display = "none";
+    document.getElementById("timerText").innerHTML = `<span id="counter" class="h4 ms-2"data-count="0"></span>`;
     document.getElementById("counters").style.display = "none";
+    document.getElementById("counter").style.display = "none";
     document.getElementById("counter").dataset.count = "0";
     document.getElementById("counter").innerText = document.getElementById("counter").dataset.count;
     document.getElementById("pageTitle").classList.add("mt-5");
     document.getElementById("leaderboardContainer").style.display = "none";
-    document.getElementById("leaderboardSortButtons").style.display = "none";
     document.getElementById("leaderboardEasy").style.display = "none";
     document.getElementById("leaderboardNormal").style.display = "none";
     document.getElementById("leaderboardHard").style.display = "none";
